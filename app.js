fc24a0e9c156250274a7ab1f347d0017842f1be3
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-03-07 12:02:16 -0800

    Refactored GrocerListItem to be a class component

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>GroceryList</h2>
+    <GroceryList groceries={['milk', 'cereal', 'eggs', 'cheese', 'bread', 'potatoes']}/>
+  </div>
+);
+
+// var GroceryListItem = (props) => {
+
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   };
+//   return (<ul>
+//     <li onClick={onListItemClick}>{props.groceries[0]}</li>
+//     <li>{props.groceries[1]}</li>
+//     <li>{props.groceries[2]}</li>
+//     <li>{props.groceries[3]}</li>
+//   </ul>
+//   );
+// };
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<li>{this.props.groceries}</li>);
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceries.map(groceries => <GroceryListItem groceries={groceries}/>)}
+  </ul>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
